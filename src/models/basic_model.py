087b09f06398a4792160cfe9a65eef3ca2cb2ffd
Nhan <nhtrnguy@ucsc.edu>
2024-02-17 22:07:10 -0800

    part 3,4,5 complete

--- src/models/basic_model.py
+++ src/models/basic_model.py
@@ -7,9 +7,43 @@ class BasicModel(Model):
     def _define_model(self, input_shape, categories_count):
         # Your code goes here
         # you have to initialize self.model to a keras model
-        pass
+        self.model = Sequential(
+        [
+            layers.Input(shape=input_shape),
+            Rescaling(1./255),
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu",input_shape=input_shape),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu",input_shape=input_shape),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu",input_shape=input_shape),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Flatten(),
+            #layers.Dense(3, activation='relu'),
+            layers.Dense(categories_count, activation="softmax"),
+        ]
+    )
+        # input = layers.Input(shape=input_shape)
+        # x = layers.Conv2D(32, (3, 3), activation='relu')(input)
+        # x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+        # x = layers.Conv2D(64, (3, 3), activation='relu')(x)
+        # x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+        # x = layers.Conv2D(128, (3, 3), activation='relu')(x)
+        # x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+        # x = layers.Flatten()(x)
+        # x = layers.Dense(128, activation='relu')(x)
+        # output_layer = layers.Dense(categories_count, activation='softmax')(x)
+
+
     
     def _compile_model(self):
         # Your code goes here
         # you have to compile the keras model, similar to the example in the writeup
-        pass
+        self.model.compile(
+            optimizer = RMSprop(learning_rate = 0.001),
+            loss = 'categorical_crossentropy',
+            metrics = ['accuracy'],
+        )
+        #self.model.print_summary()
